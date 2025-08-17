4a451031c255479a85384bf2daef126c58ac6310
varunu28 <varun.u28@gmail.com>
2025-08-17 13:41:04 -0700

    Improved debounce first test

--- circuit-breaker/debounce_test.go
+++ circuit-breaker/debounce_test.go
@@ -23,6 +23,9 @@ func TestDebounceFirst(t *testing.T) {
 		t.Errorf("Expected a valid time string, got %s", result)
 	}
 
+	// Sleep within the debounce period
+	time.Sleep(800 * time.Millisecond)
+
 	result2, err := debouncedCircuit(context.Background())
 	if err != nil {
 		t.Errorf("Expected result, got error %v", err)
@@ -32,13 +35,13 @@ func TestDebounceFirst(t *testing.T) {
 		t.Errorf("Expected a valid time string, got %s", result2)
 	}
 
-	// The second call should return the same result as the first call
+	// The second call should return the same result as the first call as it is within the debounce period
 	if firstCallTime != secondCallTime {
 		t.Errorf("Expected second call time to be the same as first call time, got %v and %v", firstCallTime, secondCallTime)
 	}
 
 	// Sleep in order to ensure the debounce period has passed
-	time.Sleep(1001 * time.Millisecond)
+	time.Sleep(300 * time.Millisecond)
 
 	// Call again. This time it should execute the circuit again
 	result3, err := debouncedCircuit(context.Background())
@@ -50,8 +53,8 @@ func TestDebounceFirst(t *testing.T) {
 		t.Errorf("Expected a valid time string, got %s", result3)
 	}
 
-	// This time we should have a different time in the result
-	if secondCallTime == thirdCallTime {
+	// This time we should have a different time in the result as the debounce period has passed
+	if firstCallTime == thirdCallTime {
 		t.Errorf("Expected third call time to be different from last call time, got %v", thirdCallTime)
 	}
 }
